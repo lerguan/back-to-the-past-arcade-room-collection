22a012e52acee9e0b5b42cae9fa5c675cf9c78fd
Leroy G <liguan698@gmail.com>
2023-05-12 12:37:03 -0700

    create addNewGame function to index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
     getGames()
+    addNewGame()
 })
 
-
+let yearList = ['1980s', '1990s', '2000s']
 function getGames() {
-    let yearList = ['1980s', '1990s', '2000s']
     for (let i of yearList) {
-        console.log(i)
         fetch(`http://localhost:3000/${i}`)
         .then(resp => resp.json())
         .then((data) => {
             data.forEach((gameData) => {
-            gameCard(gameData, i)
+            handleOneGameCard(gameData, i)
         })
         })
     }
 }
 
-function gameCard(gameData, year) {
+function handleOneGameCard(gameData, year) {
     let newImg = document.createElement('img')
-    newImg.src = `${gameData.image}`
+    newImg.src = `${gameData.flyer}`
     let newH2 = document.createElement('h2')
     newH2.setAttribute('class', 'name')
     newH2.innerText = `${gameData.name}`
+    let newGameContainer = document.createElement('div')
+    newGameContainer.setAttribute('class', 'game-container')
+    newGameContainer.appendChild(newImg)
+    // newGameContainer.appendChild(newH2)
     let yearLabel = document.getElementById(`year-${year}`)
-    yearLabel.append(newImg)
-    yearLabel.append(newH2)
-} 
+    yearLabel.append(newGameContainer)
+}
+
+function addNewGame() {
+    document.querySelector('form').addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const newGame = {
+            "name": e.target.title.value,
+            "image": e.target.image.value,
+            "flyer": e.target.flyer.value,
+            "releasedate": e.target['release-date'].value,
+            "players": e.target.players.value
+        }
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(newGame)
+        }
+        for (let year of yearList) {
+            if (Math.floor(newGame.releasedate / 10) === parseInt(year.slice(0,3))) {
+                fetch(`http://localhost:3000/${year}`, configObj)
+                .then((resp) => resp.json())
+                .then((data) => {
+                    handleOneGameCard(data, year)
+                })
+            }
+        }
+    })
+}
