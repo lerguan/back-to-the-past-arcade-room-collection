0429c733d2df08ab34ff73a93b2988874bed5d8d
Leroy G <liguan698@gmail.com>
2023-05-11 20:42:17 -0700

    create function getGames to fetch game data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-document.addEventListener()
+document.addEventListener('DOMContentLoaded', () => {
+    getGames()
+})
+
+
+function getGames() {
+    fetch('http://localhost:3000/gameList')
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(gameData => gameCard(gameData))
+    })
+}
+
+function gameCard(gameData) {
+    
+} 
