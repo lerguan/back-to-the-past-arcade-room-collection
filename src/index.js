6ff659971f64202e2dcd44f9c1fa37b5d29e3131
Leroy G <liguan698@gmail.com>
2023-05-11 23:33:10 -0700

    fetch data from db.json to index.html

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function getGames() {
-    fetch('http://localhost:3000/gameList')
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(gameData => gameCard(gameData))
-    })
+    let yearList = ['1980s', '1990s', '2000s']
+    for (let i of yearList) {
+        console.log(i)
+        fetch(`http://localhost:3000/${i}`)
+        .then(resp => resp.json())
+        .then((data) => {
+            data.forEach((gameData) => {
+            gameCard(gameData, i)
+        })
+        })
+    }
 }
 
-function gameCard(gameData) {
-    
+function gameCard(gameData, year) {
+    let newImg = document.createElement('img')
+    newImg.src = `${gameData.image}`
+    let newH2 = document.createElement('h2')
+    newH2.setAttribute('class', 'name')
+    newH2.innerText = `${gameData.name}`
+    let yearLabel = document.getElementById(`year-${year}`)
+    yearLabel.append(newImg)
+    yearLabel.append(newH2)
 } 
